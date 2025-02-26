7d4591d98e440ba3aabdc9bd4e4bdbc6ba775e43
edogola4 <edogola4@gmail.com>
2025-02-26 12:52:19 +0300

    Test passing

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+def countdown(n):
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        n -= 1
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(n):
+    import time 
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        time.sleep(1)
+        n -= 1
+    print("HAPPY NEW YEAR!")
